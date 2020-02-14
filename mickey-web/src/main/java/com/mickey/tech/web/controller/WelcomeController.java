77eb31ca8c3b1bd6e3eca4a9d3166465cedf82dd
suzhengxiao <suzhengxiao@xiaomi.com>
2020-02-14 23:39:26 +0800

    feat:后端传参至html页面

--- mickey-web/src/main/java/com/mickey/tech/web/controller/WelcomeController.java
+++ mickey-web/src/main/java/com/mickey/tech/web/controller/WelcomeController.java
@@ -2,6 +2,7 @@ package com.mickey.tech.web.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -17,7 +18,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class WelcomeController {
 
     @GetMapping("/")
-    public String index(){
+    public String index(Model model){
+        //后端传参至HTML页面
+        model.addAttribute("title", "欢迎访问系统");
         return "index";
     }
 }
