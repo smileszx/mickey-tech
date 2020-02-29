e59c8574b529555e0b3c733a1a2dfbb91d2b0c38
suzhengxiao <suzhengxiao@xiaomi.com>
2020-02-29 21:19:48 +0800

    feat: add table

---
+++ mickey-orm/src/main/java/com/mickey/tech/orm/mapper/CmsHelpMapper.java
@@ -0,0 +1,7 @@
+package com.mickey.tech.orm.mapper;
+
+import com.mickey.tech.orm.entity.CmsHelp;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface CmsHelpMapper extends Mapper<CmsHelp> {
+}
