f3b81689c5d9508450f3c7945c16f0e971a8ac06
suzhengxiao <suzhengxiao@xiaomi.com>
2020-03-02 15:38:09 +0800

    feat: add table

--- mickey-orm/src/main/java/com/mickey/tech/orm/mapper/CmsHelpMapper.java
+++ mickey-orm/src/main/java/com/mickey/tech/orm/mapper/CmsHelpMapper.java
@@ -1,7 +1,8 @@
 package com.mickey.tech.orm.mapper;
 
 import com.mickey.tech.orm.entity.CmsHelp;
+import org.springframework.stereotype.Repository;
 import tk.mybatis.mapper.common.Mapper;
-
+@Repository
 public interface CmsHelpMapper extends Mapper<CmsHelp> {
 }
